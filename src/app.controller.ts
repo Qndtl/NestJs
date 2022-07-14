b00f20cb8f0e30a9ae70b173d8fdbc2a6dd723e3
Qndtl <johd321@naver.com>
2022-07-14 17:18:22 +0900

    Add Cookie Parser

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
+import { Request, Response } from 'express';
+import { AccessJwtAuthGuard } from './auth/guards/access-jwt-auth.guard';
 
 @Controller()
 export class AppController {
   @Get('main')
-  main() {
-    return 'Main page';
+  @UseGuards(AccessJwtAuthGuard)
+  main(@Req() req: Request, @Res() res: Response) {
+    if (!req.user) return res.send('<h1>Main Page</h1>');
+    return res.send('<h1>Main Page</h1><h1>Logged In</h1>');
   }
 }
--- src/auth/strategies/access-jwt.strategy.ts
+++ src/auth/strategies/access-jwt.strategy.ts
@@ -2,6 +2,7 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 import * as dotenv from 'dotenv';
+import { Request } from 'express';
 dotenv.config();
 
 interface UserReturn {
@@ -13,7 +14,11 @@ interface UserReturn {
 export class AccessJwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (req: Request) => {
+          return req.cookies['access-token'];
+        },
+      ]),
       ignoreExpiration: false,
       secretOrKey: process.env.JWT_SECRET,
     });
